40d75941bc0cf8755e8b975998a0699aae82c1ba
Oussama Boujnan <69466099+Oussama-pixel@users.noreply.github.com>
2021-06-09 19:00:33 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Navbar from "./components/Navbar/navbar";
+import TourList from "./components/tourList";
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <TourList/>
+    </div>
+  );
+}
+
+export default App;
